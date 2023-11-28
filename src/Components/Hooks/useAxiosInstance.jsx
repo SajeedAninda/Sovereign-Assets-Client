9d549c8697eadc8e0a3b31bbe0eb28ed50294cb4
SajeedAninda <sajaninda@gmail.com>
2023-11-28 14:47:50 +0600

    Implemented Axios Interceptor

--- src/Components/Hooks/useAxiosInstance.jsx
+++ src/Components/Hooks/useAxiosInstance.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect } from 'react';
+import useAuth from './useAuth';
+import { useNavigate } from 'react-router-dom';
 
 const instance = axios.create({
     baseURL: 'http://localhost:5000',
@@ -7,6 +9,26 @@ const instance = axios.create({
 });
 
 const useAxiosInstance = () => {
+    let { logOut } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        instance.interceptors.response.use(res => {
+            return res;
+        }, error => {
+            if (error.response.status == 401) {
+                logOut()
+                    .then(() => {
+                        navigate("/login");
+                        console.log("Logged Out Successfully");
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
+        })
+    }, [])
+
     return instance;
 };
 
